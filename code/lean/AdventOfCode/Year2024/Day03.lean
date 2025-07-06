17cca7a8d5afec9f5cd86846cfd1cb29eeb1d894
Michael Khryashchev <me@xiberfix.dev>
2025-07-06 16:06:46 +0400

    lean: solve 2024.03 part 2

--- code/lean/AdventOfCode/Year2024/Day03.lean
+++ code/lean/AdventOfCode/Year2024/Day03.lean
@@ -3,26 +3,56 @@ import Regex
 namespace Year2024.Day03
 
 
-def parse (input : String) : Array (Nat × Nat) :=
-  re! r"mul\((\d+),(\d+)\)"
+inductive Op where
+  | mul (a b : Int)
+  | enable
+  | disable
+deriving Inhabited
+
+def parse (input : String) : Array Op :=
+  re! r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)"
     |>.captureAll input
-    |>.map (·.toArray)
-    |>.map (fun groups => ⟨groups[1]!.get!.toNat?.get!, groups[2]!.get!.toNat?.get!⟩)
+    |>.map (·.toArray.map (·.map (·.toString)))
+    |>.map (parseOp ·)
+  where
+    parseOp
+      | #[some "do()",    none,   none  ] => .enable
+      | #[some "don't()", none,   none  ] => .disable
+      | #[some _,         some a, some b] => .mul a.toNat! b.toNat!
+      | _ => panic! "unreachable"
 
 
 def part₁ (input : String) : Int :=
-  parse input
-    |>.map (fun (a, b) => a * b)
-    |>.sum
+  let ops := parse input
+  Id.run do
+    let mut res := 0
+    for op in ops do
+      match op with
+      | .mul a b => res := res + a * b
+      | _ => ()
+    res
 
 
 def part₂ (input : String) : Int :=
-  0
+  let ops := parse input
+  Id.run do
+    let mut res := 0
+    let mut flag := true
+    for op in ops do
+      match op with
+      | .mul a b => if flag then res := res + a * b
+      | .enable  => flag := true
+      | .disable => flag := false
+    res
 
 
-def test : String := r"
+def test₁ : String := r"
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 ".trimLeft
 
-#eval part₁ test -- 161
-#eval part₂ test -- 0
+def test₂ : String := r"
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+".trimLeft
+
+#eval part₁ test₁ -- 161
+#eval part₂ test₂ -- 48
