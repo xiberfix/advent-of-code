2b4fe3cd4cdf8841881a0ad2f466d34dc469cdfd
Michael Khryashchev <me@xiberfix.dev>
2026-06-21 12:23:34 +0400

    lean: refactor 2024.01

--- code/lean/AdventOfCode/Year2024/Day01.lean
+++ code/lean/AdventOfCode/Year2024/Day01.lean
@@ -1,26 +1,21 @@
-import Std.Data.HashMap
+import AdventOfCode.Parser
+import AdventOfCode.Utils
 
 namespace Year2024.Day01
 
-
-def parse (input : String) : List (Int × Int) :=
-  input.trim.splitOn "\n"
-    |>.map (·.splitOn "   ")
-    |>.map (fun pair => ⟨pair[0]!.toInt!, pair[1]!.toInt!⟩)
+def parser := linesOf p!"{int}   {int}"
 
 
 def part₁ (input : String) : Int :=
-  let (ls, rs) := parse input |>.unzip
-  List.zipWith dist ls.mergeSort rs.mergeSort |>.sum
-  where
-    dist a b := (a - b).natAbs
+  let (ls, rs) := input.parse parser |>.unzip
+  let dist := fun (x, y) => (x - y).natAbs
+  ls.qsort.iter.zip rs.qsort.iter |>.map dist |>.sum
 
 
 def part₂ (input : String) : Int :=
-  let (ls, rs) := parse input |>.unzip
-  let counts : Std.HashMap Int Nat := rs.foldl (init := ∅)
-    (fun acc x => acc.alter x (fun v => some (1 + v.getD 0)))
-  ls.map (fun x => x * counts.getD x 0) |>.sum
+  let (ls, rs) := input.parse parser |>.unzip
+  let counts := rs.iter.counts
+  ls.iter |>.map (fun k => k * counts.getD k 0) |>.sum
 
 
 def test : String := r"
@@ -30,7 +25,7 @@ def test : String := r"
 1   3
 3   9
 3   3
-".trimLeft
+"
 
 #eval part₁ test -- 11
 #eval part₂ test -- 31
